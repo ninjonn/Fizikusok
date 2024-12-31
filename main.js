a5bff06c74c5b9e2e4c9b8dfb5db3182ebf466d8
Dominik <szeplakidominik888@gmail.com>
2024-12-31 16:46:34 +0100

    tomb bevezetese

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const headerObj = { // A header objektum, amely a táblázat oszlopait reprezentálja
-    field1:'Fizika területe', // headerobj field1 tulajdonságának megadjuk a 'Fizika területe' értéket
-    field2:'Időszak', // headerobj field3 tulajdonságának megadjuk a 'Időszak' értéket
-    field3:'Képviselők' // headerobj field2 tulajdonságának megadjuk a 'Képviselők' értéket
-};
+const array = [
+    { // Az első objektum a táblázat fejlécének adatait tartalmazza
+        field1: 'Fizika területe', // Az első fejléc cella szövegét állítja be
+        field2: 'Időszak', // A második fejléc cella szövegét állítja be
+        field3: 'Képviselők' // A harmadik fejléc cella szövegét állítja be
+    },
 
-const row1Obj = { // Az első sor objektuma, amely az első tudományos területet és a hozzá tartozó adatokat tartalmazza
-    field1:'Optika', // row1Obj field1 tulajdonságának megadjuk a 'Optika' értéket
-    field2:'XI. század', // row1Obj field2 tulajdonságának megadjuk a 'XI. század' értéket
-    field3:'Alhazen' // row1Obj field3 tulajdonságának megadjuk a 'Alhazen' értéket
-};
+    { // Az első adatokat tartalmazó sor objektuma
+        field1: 'Optika', // // Az első cella szövegét állítja be az első sorban
+        field2: 'XI. század', // A második cella szövegét állítja be az első sorban
+        field3: 'Alhazen' // A harmadik cella szövegét állítja be az első sorban
+    },
 
-const row2Obj = { // A második sor objektuma, amely az asztronómiát és a hozzá tartozó adatokat tartalmazza
-    field1:'Asztronómia', // row2Obj field1 tulajdonságának megadjuk a 'Asztronómia' értéket
-    field2:'reneszánsz', // row2Obj field2 tulajdonságának megadjuk a 'reneszánsz' értéket
-    field3:'Kepler', // row2Obj field3 tulajdonságának megadjuk a 'Kepler' értéket
-    field4:'Galilei' // row2Obj field4 tulajdonságának megadjuk a 'Galilei' értéket
-};
+    { // A második adatokat tartalmazó sor objektuma
+        field1: 'Asztronómia', // Az első cella szövegét állítja be a második sorban
+        field2: 'reneszánsz', // A második cella szövegét állítja be a második sorban
+        field3: 'Kepler', // A harmadik cella szövegét állítja be a második sorban
+        field4: 'Galilei' // A negyedik cella szövegét állítja be a második sorban
+    },
 
-const row3Obj = { // A harmadik sor objektuma, amely a kvantumfizikát és a hozzá tartozó adatokat tartalmazza
-    field1:'Kvantumfizika', // row3Obj field1 tulajdonságának megadjuk a 'Kvantumfizika' értéket
-    field2:'XIX-XX. század', // row3Obj field2 tulajdonságának megadjuk a 'XIX-XX. század' értéket
-    field3:'Max Planck', // row3Obj field3 tulajdonságának megadjuk a 'Max Planck' értéket
-    field4:'Albert Einstein' // row3Obj field4 tulajdonságának megadjuk a 'Albert Einstein' értéket
-};
+    { // A harmadik adatokat tartalmazó sor objektuma
+        field1: 'Kvantumfizika', // Az első cella szövegét állítja be a harmadik sorban
+        field2: 'XIX-XX. század', // A második cella szövegét állítja be a harmadik sorban
+        field3: 'Max Planck', // A harmadik cella szövegét állítja be a harmadik sorban
+        field4: 'Albert Einstein' // A negyedik cella szövegét állítja be a harmadik sorban
+    },
 
-const row4Obj = { // A negyedik sor objektuma, amely a modern fizikát és a hozzá tartozó adatokat tartalmazza
-    field1:'Modern fizika', // row4Obj field1 tulajdonságának megadjuk a 'Modern fizika' értéket
-    field2:'XX-XXI. század', // row4Obj field2 tulajdonságának megadjuk a 'XX-XXI. század' értéket
-    field3:'Richard Feynman', // row4Obj field3 tulajdonságának megadjuk a 'Richard Feynman' értéket
-    field4:'Stephen Hawking' // row4Obj field4 tulajdonságának megadjuk a 'Stephen Hawking' értéket
-};
-
-
+    { // A negyedik adatokat tartalmazó sor objektuma
+        field1: 'Modern fizika', // Az első cella szövegét állítja be a negyedik sorban
+        field2: 'XX-XXI. század', // A második cella szövegét állítja be a negyedik sorban
+        field3: 'Richard Feynman', // A harmadik cella szövegét állítja be a negyedik sorban
+        field4: 'Stephen Hawking' // A negyedik cella szövegét állítja be a negyedik sorban
+    }
+];
 
 const table = document.createElement('table'); // Táblázat HTML elem létrehozása
 document.body.appendChild(table); // Hozzáadjuk a táblázatot a dokumentum törzséhez (body)
@@ -42,15 +42,15 @@ table.appendChild(tableHeader); // A fejléc (thead) hozzáadása a táblázatho
 tableHeader.appendChild(headerRow); // A fejlécsor (tr) hozzáadása a fejlécbe (thead)
 
 const headerCell1 = document.createElement('th'); // Fejléc cella (th) létrehozása
-headerCell1.innerHTML = headerObj.field1;  // A cella tartalmát beállítjuk a headerObj.field1 értékével
+headerCell1.innerHTML = array[0].field1;  // Az első fejléc cella szövegének beállítása az első objektum 'field1' értékére
 headerRow.appendChild(headerCell1); // A cella (th) hozzáadása a fejlécsorhoz (tr)
 
 const headerCell2 = document.createElement('th'); // Új fejléc cella (th) létrehozása
-headerCell2.innerHTML = headerObj.field2; // A cella tartalmát beállítjuk a headerObj.field2 értékével
+headerCell2.innerHTML = array[0].field2; // A második fejléc cella szövegének beállítása az első objektum 'field2' értékére
 headerRow.appendChild(headerCell2); // A cella hozzáadása a fejlécsorhoz
 
 const headerCell3 = document.createElement('th'); // Új fejléc cella (th) létrehozása
-headerCell3.innerHTML = headerObj.field3; // A cella tartalmát beállítjuk a headerObj.field3 értékével
+headerCell3.innerHTML = array[0].field3; // A harmadik fejléc cella szövegének beállítása az első objektum 'field3' értékére
 headerCell3.colSpan = 2; // A cella két oszlopot foglal el (colSpan beállítása)
 headerRow.appendChild(headerCell3); // A cella hozzáadása a fejlécsorhoz
 
@@ -61,16 +61,16 @@ const row1 = document.createElement('tr'); // Táblázat első sorának (tr) lé
 tableBody.appendChild(row1); // Az első sor (tr) hozzáadása a törzshöz (tbody)
 
 const row1Cell1 = document.createElement('td'); // Első cella (td) létrehozása az első sorhoz
-row1Cell1.innerHTML = row1Obj.field1; // A cella tartalmát beállítjuk a row1Obj.field1 értékével
+row1Cell1.innerHTML = array[1].field1; // Az első cella szövegének beállítása az első sor 'field1' értékére
 row1.appendChild(row1Cell1); // A cella hozzáadása az első sorhoz
 
 const row1Cell2 = document.createElement('td'); // Második cella (td) létrehozása az első sorhoz
-row1Cell2.innerHTML = row1Obj.field2; // A cella tartalmát beállítjuk a row1Obj.field2 értékével
+row1Cell2.innerHTML = array[1].field2; // A második cella szövegének beállítása az első sor 'field2' értékére
 row1.appendChild(row1Cell2); // A cella hozzáadása az első sorhoz
 
 const row1Cell3 = document.createElement('td'); // Harmadik cella (td) létrehozása az első sorhoz
 row1Cell3.colSpan = 2; // A cella két oszlopot foglal el
-row1Cell3.innerHTML = row1Obj.field3; // A cella tartalmát beállítjuk a row1Obj.field3 értékével
+row1Cell3.innerHTML = array[1].field3; // A harmadik cella szövegének beállítása az első sor 'field3' értékére
 row1.appendChild(row1Cell3); // A cella hozzáadása az első sorhoz
 
 
@@ -78,19 +78,19 @@ const row2 = document.createElement('tr'); // Második sor (tr) létrehozása
 tableBody.appendChild(row2); // A második sor hozzáadása a törzshöz
 
 const row2Cell1 = document.createElement('td'); // Első cella létrehozása a második sorhoz
-row2Cell1.innerHTML = row2Obj.field1; // A cella tartalmát beállítjuk a row2Obj.field1 értékével
+row2Cell1.innerHTML = array[2].field1; // Az első cella szövegének beállítása a második sor 'field1' értékére
 row2.appendChild(row2Cell1); // A cella hozzáadása a második sorhoz
 
 const row2Cell2 = document.createElement('td'); // Második cella létrehozása a második sorhoz
-row2Cell2.innerHTML = row2Obj.field2; // A cella tartalmát beállítjuk a row2Obj.field2 értékével
+row2Cell2.innerHTML = array[2].field2; // A második cella szövegének beállítása a második sor 'field2' értékére
 row2.appendChild(row2Cell2); // A cella hozzáadása a második sorhoz
 
 const row2Cell3 = document.createElement('td'); // Harmadik cella létrehozása a második sorhoz
-row2Cell3.innerHTML = row2Obj.field3; // A cella tartalmát beállítjuk a row2Obj.field3 értékével
+row2Cell3.innerHTML = array[2].field3; // A harmadik cella szövegének beállítása a második sor 'field3' értékére
 row2.appendChild(row2Cell3); // A cella hozzáadása a második sorhoz
 
 const row2Cell4 = document.createElement('td'); // Negyedik cella létrehozása a második sorhoz
-row2Cell4.innerHTML = row2Obj.field4; // A cella tartalmát beállítjuk a row2Obj.field4 értékével
+row2Cell4.innerHTML = array[2].field4; // A negyedik cella szövegének beállítása a második sor 'field4' értékére
 row2.appendChild(row2Cell4); // A cella hozzáadása a második sorhoz
 
 
@@ -98,19 +98,19 @@ const row3 = document.createElement('tr'); // Harmadik sor létrehozása
 tableBody.appendChild(row3); // A harmadik sor hozzáadása a törzshöz
 
 const row3Cell1 = document.createElement('td'); // Első cella létrehozása a harmadik sorhoz
-row3Cell1.innerHTML = row3Obj.field1; // A cella tartalmát beállítjuk a row3Obj.field1 értékével
+row3Cell1.innerHTML = array[3].field1; // Az első cella szövegének beállítása a harmadik sor 'field1' értékére
 row3.appendChild(row3Cell1); // A cella hozzáadása a harmadik sorhoz
 
 const row3Cell2 = document.createElement('td'); // Második cella létrehozása a harmadik sorhoz
-row3Cell2.innerHTML = row3Obj.field2; // A cella tartalmát beállítjuk a row3Obj.field2 értékével
+row3Cell2.innerHTML = array[3].field2; // A második cella szövegének beállítása a harmadik sor 'field2' értékére
 row3.appendChild(row3Cell2); // A cella hozzáadása a harmadik sorhoz
 
 const row3Cell3 = document.createElement('td'); // Harmadik cella létrehozása a harmadik sorhoz
-row3Cell3.innerHTML = row3Obj.field3; // A cella tartalmát beállítjuk a row3Obj.field3 értékével
+row3Cell3.innerHTML = array[3].field3; // A harmadik cella szövegének beállítása a harmadik sor 'field3' értékére
 row3.appendChild(row3Cell3); // A cella hozzáadása a harmadik sorhoz
 
 const row3Cell4 = document.createElement('td'); // Negyedik cella létrehozása a harmadik sorhoz
-row3Cell4.innerHTML = row3Obj.field4; // A cella tartalmát beállítjuk a row3Obj.field4 értékével
+row3Cell4.innerHTML = array[3].field4; // A negyedik cella szövegének beállítása a harmadik sor 'field4' értékére
 row3.appendChild(row3Cell4); // A cella hozzáadása a harmadik sorhoz
 
 
@@ -118,17 +118,17 @@ const row4 = document.createElement('tr'); // Negyedik sor létrehozása
 tableBody.appendChild(row4); // A negyedik sor hozzáadása a törzshöz
 
 const row4Cell1 = document.createElement('td'); // Első cella létrehozása a negyedik sorhoz
-row4Cell1.innerHTML = row4Obj.field1; // A cella tartalmát beállítjuk a row4Obj.field1 értékével
+row4Cell1.innerHTML = array[4].field1; // Az első cella szövegének beállítása a negyedik sor 'field1' értékére
 row4.appendChild(row4Cell1); // A cella hozzáadása a negyedik sorhoz
 
 const row4Cell2 = document.createElement('td'); // Második cella létrehozása a negyedik sorhoz
-row4Cell2.innerHTML = row4Obj.field2; // A cella tartalmát beállítjuk a row4Obj.field2 értékével
+row4Cell2.innerHTML = array[4].field2; // A második cella szövegének beállítása a negyedik sor 'field2' értékére
 row4.appendChild(row4Cell2); // A cella hozzáadása a negyedik sorhoz
 
 const row4Cell3 = document.createElement('td'); // Harmadik cella létrehozása a negyedik sorhoz
-row4Cell3.innerHTML = row4Obj.field3; // A cella tartalmát beállítjuk a row4Obj.field3 értékével
+row4Cell3.innerHTML = array[4].field3; // A harmadik cella szövegének beállítása a negyedik sor 'field3' értékére
 row4.appendChild(row4Cell3); // A cella hozzáadása a negyedik sorhoz
 
 const row4Cell4 = document.createElement('td'); // Negyedik cella létrehozása a negyedik sorhoz
-row4Cell4.innerHTML = row4Obj.field4; // A cella tartalmát beállítjuk a row4Obj.field4 értékével
+row4Cell4.innerHTML = array[4].field4; // A negyedik cella szövegének beállítása a negyedik sor 'field4' értékére
 row4.appendChild(row4Cell4); // A cella hozzáadása a negyedik sorhoz
